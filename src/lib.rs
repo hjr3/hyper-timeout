56888b671d19719d8ceedb32b9dbd64cd7ad4373
Herman J. Radtke III <herman@hermanradtke.com>
2019-08-08 17:22:18 -0700

    Update to work with hyper v0.12

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 extern crate futures;
-extern crate tokio_core;
+extern crate tokio;
 extern crate tokio_io;
 extern crate tokio_service;
 extern crate tokio_io_timeout;
@@ -8,22 +8,18 @@ extern crate hyper;
 use std::time::Duration;
 use std::io;
 
-use futures::future::{Either, Future};
+use futures::Future;
 
-use tokio_core::reactor::{Handle, Timeout};
-use tokio_io::{AsyncRead, AsyncWrite};
-use tokio_service::Service;
+use tokio::timer::Timeout;
 use tokio_io_timeout::TimeoutStream;
 
-use hyper::client::Connect;
+use hyper::client::connect::{Connect, Connected, Destination};
 
 /// A connector that enforces as connection timeout
 #[derive(Debug)]
 pub struct TimeoutConnector<T> {
     /// A connector implementing the `Connect` trait
     connector: T,
-    /// Handle to be used to set the timeout within tokio's core
-    handle: Handle,
     /// Amount of time to wait connecting
     connect_timeout: Option<Duration>,
     /// Amount of time to wait reading response
@@ -34,16 +30,56 @@ pub struct TimeoutConnector<T> {
 
 impl<T: Connect> TimeoutConnector<T> {
     /// Construct a new TimeoutConnector with a given connector implementing the `Connect` trait
-    pub fn new(connector: T, handle: &Handle) -> Self {
+    pub fn new(connector: T) -> Self {
         TimeoutConnector {
             connector: connector,
-            handle: handle.clone(),
             connect_timeout: None,
             read_timeout: None,
             write_timeout: None,
         }
     }
+}
 
+impl<T: Connect> Connect for TimeoutConnector<T>
+where
+    T: Connect<Error = io::Error> + 'static,
+    T::Future: 'static,
+{
+    type Transport = TimeoutStream<T::Transport>;
+    type Error = T::Error;
+    type Future = Box<Future<Item = (Self::Transport, Connected), Error = Self::Error> + Send>;
+
+    fn connect(&self, dst: Destination) -> Self::Future {
+
+        let read_timeout = self.read_timeout.clone();
+        let write_timeout = self.write_timeout.clone();
+        let connecting = self.connector.connect(dst);
+
+        if self.connect_timeout.is_none() {
+            return Box::new(connecting.map(move |(io, c)| {
+                let mut tm = TimeoutStream::new(io);
+                tm.set_read_timeout(read_timeout);
+                tm.set_write_timeout(write_timeout);
+                (tm, c)
+            }));
+        }
+
+        let connect_timeout = self.connect_timeout.expect("Connect timeout should be set");
+        let timeout = Timeout::new(connecting, connect_timeout);
+
+        Box::new(timeout.then(move |res| match res {
+            Ok((io, c)) => {
+                let mut tm = TimeoutStream::new(io);
+                tm.set_read_timeout(read_timeout);
+                tm.set_write_timeout(write_timeout);
+                Ok((tm, c))
+            }
+            Err(e) => Err(io::Error::new(io::ErrorKind::TimedOut, e)),
+        }))
+    }
+}
+
+impl<T> TimeoutConnector<T> {
     /// Set the timeout for connecting to a URL.
     ///
     /// Default is no timeout.
@@ -69,76 +105,70 @@ impl<T: Connect> TimeoutConnector<T> {
     }
 }
 
-impl<T> Service for TimeoutConnector<T>
-where
-    T: Service<Error = io::Error> + 'static,
-    T::Response: AsyncRead + AsyncWrite,
-    T::Future: Future<Error = io::Error>,
-{
-    type Request = T::Request;
-    type Response = TimeoutStream<T::Response>;
-    type Error = T::Error;
-    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
-
-    fn call(&self, req: Self::Request) -> Self::Future {
-        let handle = self.handle.clone();
-        let read_timeout = self.read_timeout.clone();
-        let write_timeout = self.write_timeout.clone();
-        let connecting = self.connector.call(req);
-
-        if self.connect_timeout.is_none() {
-            return Box::new(connecting.map(move |io| {
-                let mut tm = TimeoutStream::new(io, &handle);
-                tm.set_read_timeout(read_timeout);
-                tm.set_write_timeout(write_timeout);
-                tm
-            }));
-        }
-
-        let connect_timeout = self.connect_timeout.expect("Connect timeout should be set");
-        let timeout = Timeout::new(connect_timeout, &self.handle).unwrap();
-
-        Box::new(connecting.select2(timeout).then(move |res| match res {
-            Ok(Either::A((io, _))) => {
-                let mut tm = TimeoutStream::new(io, &handle);
-                tm.set_read_timeout(read_timeout);
-                tm.set_write_timeout(write_timeout);
-                Ok(tm)
-            }
-            Ok(Either::B((_, _))) => {
-                Err(io::Error::new(
-                    io::ErrorKind::TimedOut,
-                    "Client timed out while connecting",
-                ))
-            }
-            Err(Either::A((e, _))) => Err(e),
-            Err(Either::B((e, _))) => Err(e),
-        }))
-    }
-}
-
 #[cfg(test)]
 mod tests {
+    use std::error::Error;
     use std::io;
     use std::time::Duration;
-    use tokio_core::reactor::Core;
-    use hyper::client::{Connect, HttpConnector};
+    use futures::future;
+    use tokio::runtime::current_thread::Runtime;
+    use hyper::Client;
+    use hyper::client::HttpConnector;
     use super::TimeoutConnector;
 
     #[test]
     fn test_timeout_connector() {
-        let mut core = Core::new().unwrap();
-        // 10.255.255.1 is a not a routable IP address
-        let url = "http://10.255.255.1".parse().unwrap();
-        let mut connector =
-            TimeoutConnector::new(HttpConnector::new(1, &core.handle()), &core.handle());
-        connector.set_connect_timeout(Some(Duration::from_millis(1)));
+        let mut rt = Runtime::new().unwrap();
+        let res = rt.block_on(future::lazy(|| {
+            // 10.255.255.1 is a not a routable IP address
+            let url = "http://10.255.255.1".parse().unwrap();
 
-        match core.run(connector.connect(url)) {
+            let http = HttpConnector::new(1);
+            let mut connector = TimeoutConnector::new(http);
+            connector.set_connect_timeout(Some(Duration::from_millis(1)));
+
+            let client = Client::builder().build::<_, hyper::Body>(connector);
+
+            client.get(url)
+        }));
+
+        match res {
+            Ok(_) => panic!("Expected a timeout"),
             Err(e) => {
-                assert_eq!(e.kind(), io::ErrorKind::TimedOut);
+                if let Some(io_e) = e.source().unwrap().downcast_ref::<io::Error>() {
+                    assert_eq!(io_e.kind(), io::ErrorKind::TimedOut);
+                } else {
+                    panic!("Expected timeout error");
+                }
             }
-            _ => panic!("Expected timeout error"),
+        }
+    }
+
+    #[test]
+    fn test_read_timeout() {
+        let mut rt = Runtime::new().unwrap();
+        let res = rt.block_on(future::lazy(|| {
+            let url = "http://example.com".parse().unwrap();
+
+            let http = HttpConnector::new(1);
+            let mut connector = TimeoutConnector::new(http);
+            // A 1 ms read timeout should be so short that we trigger a timeout error
+            connector.set_read_timeout(Some(Duration::from_millis(1)));
+
+            let client = Client::builder().build::<_, hyper::Body>(connector);
+
+            client.get(url)
+        }));
+
+        match res {
+            Ok(_) => panic!("Expected a timeout"),
+            Err(e) => {
+                if let Some(io_e) = e.source().unwrap().downcast_ref::<io::Error>() {
+                    assert_eq!(io_e.kind(), io::ErrorKind::TimedOut);
+                } else {
+                    panic!("Expected timeout error");
+                }
+            }
         }
     }
 }
